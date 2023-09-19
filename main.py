47a6db21b5cb368f2bb43d4f6a68a179d5219f63
jurchiks33 <jurisslisans@gmail.com>
2023-09-19 12:30:45 +0200

    Introduced first 5 skills

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ class SimpleWindow:
         size = "{}x{}".format(int(width_in_pixel), int(height_in_pixel))
         self.master.geometry(size)
 
-        label = tk.Label(self.master, text="IDLE MASTER", font=("Arial", 24, "bold"), bg="grey", fg="black")
-        label.pack(pady=20)
+        label_title = tk.Label(self.master, text="IDLE MASTER", font=("Arial", 24, "bold"), bg="grey", fg="black")
+        label_title.pack(pady=20)
 
         names = ["sword", "fists", "bow", "fireball", "frostbolt"]
 
@@ -25,12 +25,27 @@ class SimpleWindow:
         left_container.pack(side=tk.LEFT, padx=5, pady=5)
 
         for name in names:
+            value = tk.StringVar()
+            value.set("1")
+
             frame = tk.Frame(left_container, bg="white", width=frame_width, height=frame_height)
             frame.pack_propagate(0)
             frame.pack(side=tk.TOP, pady=5)
 
-            label = tk.Label(frame, text=name, bg="white", font=("Arial", 12, "bold"))
+            label = tk.Label(frame, text=name + ": 1", bg="white", font=("Arial", 12, "bold"), takefocus=0)
             label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+            frame.bind("<Enter>", lambda e, f=frame, l=label: (f.configure(bg="lightblue"), l.configure(bg="lightblue")), add='+')
+            frame.bind("<Leave>", lambda e, f=frame, l=label: (f.configure(bg="white"), l.configure(bg="white")), add='+')
+            frame.bind("<ButtonPress-1>", lambda event, v=value, lbl=label, nm=name: self.increase_value(v, lbl, nm))
+            label.bind("<ButtonPress-1>", lambda event, v=value, lbl=label, nm=name: self.increase_value(v, lbl, nm))
+
+           
+    def increase_value(self, value, label, name):
+        current_value = int(value.get())
+        new_value = str(current_value + 1)
+        value.set(new_value)
+        label.config(text=name + ": " + new_value)
 
 if __name__ == "__main__":
     root = tk.Tk()
