2e0fceb2177ff09a74d8485446b26172a22555b4
jurchiks33 <jurisslisans@gmail.com>
2023-09-19 09:52:29 +0200

    Adding 5 skils to master

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ class SimpleWindow:
         label = tk.Label(self.master, text="IDLE MASTER", font=("Arial", 24, "bold"), bg="grey", fg="black")
         label.pack(pady=20)
 
+        names = ["sword", "fists", "bow", "fireball", "frostbolt"]
+
+        frame_width = int((5 / 2.54) * 96)
+        frame_height = int((2 / 2.54) * 96)
+
+        left_container = tk.Frame(self.master, bg="grey")
+        left_container.pack(side=tk.LEFT, padx=5, pady=5)
+
+        for name in names:
+            frame = tk.Frame(left_container, bg="white", width=frame_width, height=frame_height)
+            frame.pack_propagate(0)
+            frame.pack(side=tk.TOP, pady=5)
+
+            label = tk.Label(frame, text=name, bg="white", font=("Arial", 12, "bold"))
+            label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
 if __name__ == "__main__":
     root = tk.Tk()
     window = SimpleWindow(root)
