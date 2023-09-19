57d702eaeff2a0ae985705e2b21839aaa55abe54
jurchiks33 <jurisslisans@gmail.com>
2023-09-19 09:21:22 +0200

    Some Cosmetic Changes

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ class SimpleWindow:
         self.master = master
         self.master.title("Simple Game Window")
 
+        self.master.configure(bg="grey")
+
         width_in_pixel = (35 / 2.54) * 96
         height_in_pixel = (25 / 2.54) * 96
 
         size = "{}x{}".format(int(width_in_pixel), int(height_in_pixel))
         self.master.geometry(size)
+
+        label = tk.Label(self.master, text="IDLE MASTER", font=("Arial", 24, "bold"), bg="grey", fg="black")
+        label.pack(pady=20)
 
 if __name__ == "__main__":
     root = tk.Tk()
